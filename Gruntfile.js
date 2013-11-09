12cda4543f30cdbaa7d60cc05fa7bd13dfe37b51
Antal István Miklós <istvan.m.antal@gmail.com>
2013-11-09 13:52:40 +0000

    Fine tuned jshint settings for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['Gruntfile.js', 'src/*/*.js'],
+            files: ['Gruntfile.js', 'src/*/*.js', 'tests/*/*Spec.js'],
             options: {
                 jshintrc: '.jshintrc'
             }
