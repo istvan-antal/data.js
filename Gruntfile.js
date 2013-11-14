2b323673db281e1ff09b1172806125a46bb9e352
Antal István Miklós <istvan.m.antal@gmail.com>
2013-11-14 13:16:49 +0000

    Removed desktop notifications

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,34 +20,11 @@ module.exports = function(grunt) {
                     specs: 'tests/*/*Spec.js'
                 }
             }
-        },
-        watch: {
-            scripts: {
-                files: jsFiles,
-                tasks: ['desktop-check'],
-                options: {
-                    spawn: false
-                }
-            }
         }
-    });
-    
-    grunt.task.registerTask('desktop-check', function () {
-        var done = this.async();
-        exec('grunt check', function (error, result) {
-            if (error) {
-                growl('Checks failed', { title: 'Failure', sticky: true });
-            } else {
-                growl('Tests completed successfully.', { title: 'Success', image: 'success.png', sticky: true });
-            }
-            
-            done();
-        });
     });
 
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks('grunt-contrib-jasmine');
-    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask("check", ["jshint", "jasmine"]);
     grunt.registerTask("default", ["check"]);
